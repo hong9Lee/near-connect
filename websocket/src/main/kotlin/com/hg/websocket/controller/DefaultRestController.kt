d2a606ccf357532e20638bb205e7df42c88927dd
hong9Lee <hongtemp9mac@gmail.com>
2024-10-10 20:45:15 +0900

    feat: DefaultWebsocketController 추가

--- websocket/src/main/kotlin/com/hg/websocket/controller/DefaultRestController.kt
+++ websocket/src/main/kotlin/com/hg/websocket/controller/DefaultRestController.kt
@@ -1,15 +1,19 @@
 package com.hg.websocket.controller
 
+import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.http.ResponseEntity
 import org.springframework.http.ResponseEntity.ok
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RestController
+
+private val logger = KotlinLogging.logger {}
 
 @RestController
 class DefaultRestController {
 
     @GetMapping("/ping")
     fun healthCheck(): ResponseEntity<Void> {
+        logger.info { "pong" }
         return ok().build()
     }
 }
