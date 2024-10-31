7cc2e080fd0db5dfd93e437137aa2334f25ceffe
hong9Lee <hongtemp9mac@gmail.com>
2024-10-31 21:51:41 +0900

    feat: 로그인 후, websocket session 초기화 api controller 생성

---
+++ apps/websocket/src/main/kotlin/com/hg/nc/controller/SessionInitializationController.kt
@@ -0,0 +1,20 @@
+package com.hg.nc.controller
+
+import com.hg.nc.controller.data.SessionInitializationRequest
+import com.hg.nc.service.SessionInitializationService
+import com.hg.nc.supports.UrlConstants.Companion.웹소켓_세션_초기화
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class SessionInitializationController(
+    private val sessionInitializationService: SessionInitializationService
+) {
+
+    @GetMapping(웹소켓_세션_초기화)
+    fun init(
+        request: SessionInitializationRequest
+    ) {
+        sessionInitializationService.init(request.userId)
+    }
+}
---
+++ apps/websocket/src/main/kotlin/com/hg/nc/supports/UrlConstants.kt
@@ -0,0 +1,7 @@
+package com.hg.nc.supports
+
+class UrlConstants {
+    companion object {
+        const val 웹소켓_세션_초기화 = "/websocket/v1/init"
+    }
+}
